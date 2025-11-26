2ef99934dabd0bbc6f8d1a730b3f273ed4e866e6
admin <admin@admins-Mac-mini.local>
2025-11-26 13:12:12 +0100

    Version update regarding feedbacks

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ import sys, site
 import subprocess
 import importlib
 
-# Params
+
 BASE_URL = "https://api.appshield.kobil.com"
 
+def is_valid_int(x):
+    try:
+        int_x = int(x)
+        return int_x
+    except:
+        return False
+    
 def print_colored(message, level="info"):
     """
     Prints a colored message to the console using ANSI escape codes.
@@ -35,18 +42,17 @@ def set_env_var_in_file(key, value):
         line = key + "=" + value + "\n"
         print_colored(f"Key: {key}, Value: {value}, Path: {env_path}")
 
-        # Write back
         with open(env_path, "a+") as f:
             f.write(line)
 
         print_colored(f"✅ Updated {key} in {env_path}", level="success")
+        return True
     except Exception as e:
-        print_colored(f"@@[error] [SET_ENV_VAR_IN_FILE] ❌ Failed to update {key} in {env_path}: {e}", level="error")
+        print_colored(f"@@[error] [SET_ENV_VAR_IN_FILE] Error: ❌ Failed to update {key} in {env_path}: {e}", level="error")
         return None
 
 
-# Upload & Poll helpers
-def upload_and_start_test(file_path, user_email, api_key):
+def upload_and_start_test(file_path, user_email, api_key, upload_timeout):
     try:
         upload_url = urljoin(BASE_URL, "/upload_and_test_app_parallel")
         headers = {"secret-key": api_key}
@@ -56,37 +62,42 @@ def upload_and_start_test(file_path, user_email, api_key):
 
         if not os.path.exists(file_path):
             raise Exception(f"Error: File not found at {file_path}")
+        
 
-        files = None
-        
-        f = open(file_path, "rb")
-        files = {"file": (os.path.basename(file_path), f)}
-        
-        print_colored(f"[UPLOAD_AND_START_TEST] Uploading {file_path} to {upload_url}...")
-        response = requests.post(upload_url, headers=headers, data=data, files=files, timeout=300, verify=False)
-        response.raise_for_status()
-        
-        print_colored(f"[UPLOAD_AND_START_TEST] Successfully uploaded {file_path} to {upload_url}", level="success")
-
-        response_data = response.json()
-        session_id = response_data.get("session_id")
-        if not session_id:
-            raise Exception(f"Error: Failed to start test. Response: {response_data}")
-        
-        estimated_wait_minutes = 0
-        if "queued" in response_data.get("message", "").lower():
-            estimated_wait_minutes = response_data.get("estimated_wait_minutes", 0)
-            print_colored(f"[UPLOAD_AND_START_TEST] Test queued. Estimated wait time: {estimated_wait_minutes} minutes")
+        with open(file_path, "rb") as f:
+            files = {"file": (os.path.basename(file_path), f)}
     
-        else:
-            print_colored(f"[UPLOAD_AND_START_TEST] Successfully started test session: {session_id}", level="success")
-        return session_id, estimated_wait_minutes
+            print_colored(f"[UPLOAD_AND_START_TEST] Uploading {file_path} to {upload_url}...")
+            response = requests.post(upload_url, headers=headers, data=data, files=files, timeout=upload_timeout)
+            response.raise_for_status()
+            
+            print_colored(f"[UPLOAD_AND_START_TEST] Successfully uploaded {file_path} to {upload_url}", level="success")
+
+            response_data = response.json()
+            session_id = response_data.get("session_id")
+            if not session_id:
+                raise Exception(f"Error: Failed to start test. Response: {response_data}")
+            
+            max_wait_seconds = int(response_data.get("max_wait_seconds"))
+            if not max_wait_seconds:
+                raise Exception(f"Error: Failed to start test. Response: {response_data}")
+            
+            estimated_wait_seconds = response_data.get("estimated_wait_seconds", 0)
+            if "queued" in response_data.get("message", "").lower():
+                print_colored(f"[UPLOAD_AND_START_TEST] Test queued. Estimated wait time: {estimated_wait_seconds} seconds")
+            else:
+                print_colored(f"[UPLOAD_AND_START_TEST] Successfully started test session: {session_id}", level="success")
+            return {
+                "session_id": session_id,
+                "max_wait_seconds": max_wait_seconds
+            }
 
     except requests.exceptions.Timeout:
-        raise Exception("UPLOAD_TIMEOUT: Upload request timed out")
+        raise Exception("UPLOAD TIMEOUT: Upload request timed out")
 
     except requests.exceptions.HTTPError as e:
         status = e.response.status_code if e.response is not None else "unknown"
+        print_colored(f"@@[error] [UPLOAD_AND_START_TEST] Error: HTTP {status} error", level="error")
         try:
             err_json = e.response.json()
             message = err_json.get("message", "").lower()
@@ -94,37 +105,32 @@ def upload_and_start_test(file_path, user_email, api_key):
                 print_colored("App already protected by KOBIL.")
                 return True
         except Exception:
-            # Fallback if JSON parse fails
             pass
 
         return None
-
-    except requests.exceptions.RequestException as e:
-        print_colored(f"@@[error] [UPLOAD_AND_START_TEST] Request exception during upload: {str(e)}", level="error")
-        return None
-
-    except Exception as e:
-        print_colored(f"@@[error] [UPLOAD_AND_START_TEST] Unexpected error during upload: {str(e)}", level="error")
+    
+    except (requests.exceptions.RequestException, Exception) as e:
+        print_colored(f"@@[error] [UPLOAD_AND_START_TEST] Error: {type(e).__name__}: {str(e)}", level="error")
         return None
     finally:
-        if f:
-            f.close()
+        if f is not None:
+            try:
+                f.close()
+            except Exception:
+                pass
 
-def poll_session_status(session_id, estimated_wait_minutes, api_key, poll_interval=10):
+def poll_session_status(session_id, max_wait_seconds, api_key):
     
     status_url = urljoin(BASE_URL, f"/get-session-status?session_id={session_id}")
     headers = {"secret-key": api_key}
     status_arr = ["queued", "active", "completed", "not exist"]
 
-    max_wait_seconds = estimated_wait_minutes*60 if estimated_wait_minutes > 0 else 60000
+    DEFAULT_POLL_INTERVAL_SECONDS = 10
     wait_time_seconds = 0
 
-    while True:
+    while wait_time_seconds < max_wait_seconds:
         try:
-            if wait_time_seconds > max_wait_seconds:
-                raise Exception(f"Max wait time ({max_wait_seconds} seconds) exceeded for session {session_id}")
-
-            response = requests.get(status_url, headers=headers, timeout=60, verify=False)
+            response = requests.get(status_url, headers=headers, timeout=60)
             response.raise_for_status()
             
             status_data = response.json()
@@ -141,29 +147,31 @@ def poll_session_status(session_id, estimated_wait_minutes, api_key, poll_interv
                 return True
 
             print_colored(f"[POLL_SESSION_STATUS] Session {session_id} status: {status}, waiting...")
-            time.sleep(poll_interval)
-            wait_time_seconds += poll_interval
+            
         
         except requests.exceptions.RequestException as e:
             print_colored(f"[POLL_SESSION_STATUS] An error occurred while polling for status: {str(e)}", level='warn')
-            print_colored(f"[POLL_SESSION_STATUS] Retrying in {poll_interval} seconds...")
-            time.sleep(poll_interval)
-            wait_time_seconds += poll_interval
+            print_colored(f"[POLL_SESSION_STATUS] Retrying in {DEFAULT_POLL_INTERVAL_SECONDS} seconds...")
+
         except Exception as e:
-            print_colored(f"@@[error] [POLL_SESSION_STATUS] Unexpected error during poll: {str(e)}", level="error")
+            print_colored(f"@@[error] [POLL_SESSION_STATUS] Error: Unexpected error during poll: {str(e)}", level="error")
             return None
 
-# Fetch results directly from DB
+        time.sleep(DEFAULT_POLL_INTERVAL_SECONDS)
+        wait_time_seconds += DEFAULT_POLL_INTERVAL_SECONDS
+    
+    print_colored(f"@@[error] [POLL_SESSION_STATUS] Error: Polling for session {session_id} timed out", level="error")
+    return None
+
 def get_session_results(session_id, api_key):
     status_url = urljoin(BASE_URL, f"/get-session-results?session_id={session_id}")
     headers = {"secret-key": api_key}
     try:
         print_colored(f"Fetching test results for session {session_id}...")
-        response = requests.get(status_url, headers=headers, timeout=60, verify=False)
+        response = requests.get(status_url, headers=headers, timeout=60)
         response.raise_for_status()
         
         status_data = response.json()
-        # print_colored(status_data)
         status = status_data.get("status")
 
         if status != "completed":
@@ -176,56 +184,58 @@ def get_session_results(session_id, api_key):
         return is_app_secure
 
     except requests.exceptions.RequestException as e:
-        print_colored(f"@@[error] [GET_SESSION_RESULTS] An error occurred while polling for status: {str(e)}", level='error')
+        print_colored(f"@@[error] [GET_SESSION_RESULTS] Error: An error occurred while polling for status: {str(e)}", level='error')
         return None
     except Exception as e:
-        print_colored(f"@@[error] [GET_SESSION_RESULTS] Unexpected error during fetching session results: {str(e)}", level="error")
+        print_colored(f"@@[error] [GET_SESSION_RESULTS] Error: Unexpected error during fetching session results: {str(e)}", level="error")
         return None
 
     
 
 # Per-test runner
-def run_scanner(file_path=None, user_email=None, api_key=None):
+def run_scanner(file_path=None, user_email=None, api_key=None, upload_timeout=300):
     is_app_secure = None
     try:
-        # Check parameters first
-        if not file_path:
-            raise Exception("FILE_PATH_REQUIRED: File path is required")
-        
-        if not api_key:
-            raise Exception("api_key_REQUIRED: Secret key is required")
-        
         if not user_email:
-            user_email = "efe.erdil@kobil.com"
-            print_colored(f"[RUN_SCANNER] User e-mail not provided, using default: {user_email}", level="warn")
+            print_colored(f"[RUN_SCANNER] User e-mail not provided, no mail will be sent...", level="warn")
         
-        if not os.path.exists(file_path):
-            raise Exception(f"FILE_NOT_FOUND: File {file_path} not found")
+    
+        size = os.path.getsize(file_path)
+        if not size or size == 0:
+            raise Exception(f"Not a valid file: {file_path}")
+        print_colored(f"[RUN_SCANNER] File size: {size} bytes")
+      
         
-        session_id, estimated_wait_minutes = upload_and_start_test(
+        resp = upload_and_start_test(
             file_path=file_path,
             user_email=user_email,
-            api_key=api_key
+            api_key=api_key,
+            upload_timeout=upload_timeout
         )
 
-        # App protected case
-        if session_id is True:
+        if resp is True:
             print_colored("App already protected by KOBIL.")
             is_app_secure = True
             return True
-        
-        # Upload failed due to some exception
-        if not session_id:
-            raise Exception("UPLOAD_FAILED")
 
-        res = poll_session_status(session_id, estimated_wait_minutes, api_key=api_key)
+        if not resp:
+            raise Exception("UPLOAD AND START TEST FAILED: no response")
+        
+
+        session_id = resp.get("session_id")
+        max_wait_seconds = resp.get("max_wait_seconds")
+
+        if not session_id or not max_wait_seconds:
+            raise Exception("UPLOAD AND START TEST FAILED: no session_id or max_wait_seconds")
+
+        res = poll_session_status(session_id, max_wait_seconds, api_key=api_key)
         if not res:
-            raise Exception("GET_SESSION_STATUS_FAILED")
+            raise Exception("GET SESSION STATUS FAILED")
         
         is_app_secure = get_session_results(session_id, api_key=api_key)
 
         if is_app_secure is None:
-            raise Exception("GET_SESSION_RESULTS_FAILED")
+            raise Exception("GET SESSION RESULTS FAILED")
         
         print_colored("Test OK, app is secured" if is_app_secure else "Test OK, app is NOT secured", level="success")
 
@@ -236,7 +246,6 @@ def run_scanner(file_path=None, user_email=None, api_key=None):
         return is_app_secure
     except Exception as e:
         print_colored(f"@@[error] [RUN_SCANNER] Error: {str(e)}", level="error")
-        sys.exit(1)
         return None
     finally:
         print_colored(f"Setting AC_APPSHIELD_IS_APP_SECURE to {is_app_secure}")
@@ -252,12 +261,10 @@ if __name__ == "__main__":
             "requests", "urllib3", "--break-system-packages", "--user"
         ], check=True)
 
-        # reload import system (optional but safer)
         importlib.invalidate_caches()
 
         sys.path.append(site.getusersitepackages())
 
-        # now import
         import requests, urllib3
 
         print("✅ Requests and urllib3 imported successfully!", requests.__version__, urllib3.__version__)
@@ -265,11 +272,16 @@ if __name__ == "__main__":
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
         DEFAULT_FILE_PATH = os.getenv("AC_APPSHIELD_APP_FILE_PATH")
-        DEFAULT_EMAIL = os.getenv("AC_APPSHIELD_USER_MAIL")
         API_KEY = os.getenv("AC_APPSHIELD_API_KEY")
 
-        # print(DEFAULT_EMAIL, DEFAULT_FILE_PATH)
-        print_colored(run_scanner(file_path=DEFAULT_FILE_PATH, user_email=DEFAULT_EMAIL, api_key=API_KEY))
+        DEFAULT_EMAIL = os.getenv("AC_APPSHIELD_USER_MAIL") or ""
+        DEFAULT_UPLOAD_TIMEOUT = is_valid_int(os.getenv("AC_APPSHIELD_UPLOAD_TIMEOUT")) or 300
+
+        
+        res = run_scanner(file_path=DEFAULT_FILE_PATH, user_email=DEFAULT_EMAIL, api_key=API_KEY, 
+                        upload_timeout=DEFAULT_UPLOAD_TIMEOUT)
+        if not res:
+            raise Exception("RUN SCANNER FAILED")
 
     except Exception as e:
         print_colored(f"@@[error] [MAIN] Error: {str(e)}", level="error")
